d93c1eb123998e9a1a93f60921347b88fc3ab4f4
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-12-04 13:00:42 -0500

    login path integrated with front end

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,25 @@
 const signupFormHandler = async (event) => {
-  event.preventDefault();
+  event.preventDefault()
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const email = document.querySelector('#email_signup').value.trim()
+  const username = document.querySelector('#username_signup').value.trim()
+  const password = document.querySelector('#password_signup').value.trim()
 
-  if (username && password) {
+  if (email && username && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ email, username, password }),
       headers: { 'Content-Type': 'application/json' },
-    });
+    })
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/signup')
     } else {
-      alert('Failed to sign up.');
+      alert('Failed to sign up.')
     }
   }
-};
+}
 
 document
   .querySelector('.signup_form')
-  .addEventListener('submit', signupFormHandler);
+  .addEventListener('submit', signupFormHandler)
