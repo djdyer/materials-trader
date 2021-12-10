32123ffd51b4b7275eddb2c7851b41c47b3d369e
David Dyer <davidrossdyer@gmail.com>
2021-12-10 16:10:14 -0500

    search nearly working

--- public/js/search.js
+++ public/js/search.js
@@ -1,62 +1,63 @@
-const plastic = document.querySelector('#search-plastic')
-const glass = document.querySelector('#search-glass')
-const aluminum = document.querySelector('#search-aluminum')
-const textiles = document.querySelector('#search-textiles')
-const cardboard = document.querySelector('#search-cardboard')
-const paper = document.querySelector('#search-paper')
-const electronics = document.querySelector('#search-electronics')
-const batteries = document.querySelector('#search-batteries')
-const appliances = document.querySelector('#search-appliances')
-const steel = document.querySelector('#search-steel')
-const concrete = document.querySelector('#search-concrete')
-const oil = document.querySelector('#search-oil')
+const plastic = document.querySelector('#search-plastic').value
+const glass = document.querySelector('#search-glass').value
+const aluminum = document.querySelector('#search-aluminum').value
+const textiles = document.querySelector('#search-textiles').value
+const cardboard = document.querySelector('#search-cardboard').value
+const paper = document.querySelector('#search-paper').value
+const electronics = document.querySelector('#search-electronics').value
+const batteries = document.querySelector('#search-batteries').value
+const appliances = document.querySelector('#search-appliances').value
+const steel = document.querySelector('#search-steel').value
+const concrete = document.querySelector('#search-concrete').value
+const oil = document.querySelector('#search-oil').value
 
-const searchPlastic = async (event) => {
-  const response = await fetch('/api/users', {
+
+const searchAluminum = async (event) => {
+  const response = await fetch(`/searchmaterial/3`, {
     method: 'GET',
-    body: JSON.stringify({ listings }),
+    // body: JSON.stringify({ id }),
     headers: { 'Content-Type': 'application/json' },
   })
 
   if (response.ok) {
-    document.location.replace('/home')
+    document.location.replace('/results')
   } else {
     alert('Failed to search material.')
   }
 }
 
-document
-  .querySelector('#search-plastic')
-  .addEventListener('click', searchPlastic)
+// document
+//   .querySelector('#search-plastic')
+//   .addEventListener('click', searchPlastic)
 
-document.querySelector('#search-glass')
-.addEventListener('click', searchGlass)
+// document.querySelector('#search-glass')
+// .addEventListener('click', searchGlass)
 document
   .querySelector('#search-aluminum')
-  .addEventListener('click', searchAluminum)
-document
-  .querySelector('#search-textiles')
-  .addEventListener('click', searchTextiles)
-document
-  .querySelector('#search-cardboard')
-  .addEventListener('click', searchCardboard)
+  .addEventListener('click', searchAluminum);
+// document
+//   .querySelector('#search-textiles')
+//   .addEventListener('click', searchTextiles)
+// document
+//   .querySelector('#search-cardboard')
+//   .addEventListener('click', searchCardboard)
 
-document.querySelector('#search-paper')
-.addEventListener('click', searchPaper)
-document
-  .querySelector('#search-electronics')
-  .addEventListener('click', searchElectronics)
-document
-  .querySelector('#search-batteries')
-  .addEventListener('click', searchBatteries)
-document
-  .querySelector('#search-appliances')
-  .addEventListener('click', searchAppliances)
+// document.querySelector('#search-paper')
+// .addEventListener('click', searchPaper)
+// document
+//   .querySelector('#search-electronics')
+//   .addEventListener('click', searchElectronics)
+// document
+//   .querySelector('#search-batteries')
+//   .addEventListener('click', searchBatteries)
+// document
+//   .querySelector('#search-appliances')
+//   .addEventListener('click', searchAppliances)
 
-document.querySelector('#search-steel')
-.addEventListener('click', searchSteel)
-document
-  .querySelector('#search-concrete')
-  .addEventListener('click', searchConcrete)
-document.querySelector('#search-oil')
-.addEventListener('click', searchOil)
+// document.querySelector('#search-steel')
+// .addEventListener('click', searchSteel)
+// document
+//   .querySelector('#search-concrete')
+//   .addEventListener('click', searchConcrete)
+// document.querySelector('#search-oil')
+// .addEventListener('click', searchOil)
