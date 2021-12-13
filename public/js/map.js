46d563577d4e76bbce6d28a6028c55cb18bee5f4
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-12-13 12:35:33 -0500

    creating api, map.js and creating about.page

---
+++ public/js/map.js
@@ -0,0 +1,36 @@
+// require('dotenv').config()
+
+const API_KEY = 'AIzaSyD-RuWSCkmZwh_RKF5GZKhWWkbbwVKkrdQ'
+
+console.log('MAP JS CONNECTED')
+
+function mapApiCall() {
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${API_KEY}`
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('GOOGLE MAP LOCATION DATA', data.results[0].geometry.location)
+
+      let coords = {
+        lat: data.results[0].geometry.location.lat,
+        lon: data.results[0].geometry.location.lng,
+      }
+
+      myMap(coords)
+    })
+}
+
+mapApiCall()
+
+function myMap(coords) {
+  console.log('coords in my Map', coords)
+  var mapProp = {
+    center: new google.maps.LatLng(51.508742, -0.12085),
+    zoom: 5,
+  }
+  var map = new google.maps.Map(
+    document.getElementById('location-map'),
+    mapProp,
+  )
+}
