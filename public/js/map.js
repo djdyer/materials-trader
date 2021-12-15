fd1386c86f42f58e55892db8f13fc0900fb073db
David Dyer <davidrossdyer@gmail.com>
2021-12-15 11:29:32 -0500

    added more seeds, put static map back for now

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
 const API_KEY = 'AIzaSyD-RuWSCkmZwh_RKF5GZKhWWkbbwVKkrdQ'
 
-const address = document.querySelector('#material-address').value
-console.log("address:", address);
+// const address = document.querySelector('#material-address').value
+// console.log("address:", address);
 
 const address = "Atlanta";
 console.log('MAP JS CONNECTED')
@@ -15,29 +15,29 @@ function mapApiCall() {
       console.log('GOOGLE MAP LOCATION DATA', data.results[0].geometry.location)
 
       let coords = {
-        latitude: data.results[0].geometry.location.lat,
-        longitude: data.results[0].geometry.location.lng,
+        lat: data.results[0].geometry.location.lat,
+        lng: data.results[0].geometry.location.lng,
       }
 
-      // myMap(coords);
+      myMap(coords);
      
-        new google.maps.Map(document.getElementById('location-map'), {
-          center: {lat: coords.latitude, lng: coords.longitude},
-          zoom: 5
-        });
+        // new google.maps.Map(document.getElementById('location-map'), {
+        //   center: {lat: coords.latitude, lng: coords.longitude},
+        //   zoom: 5
+        // });
     })
 }
 
 mapApiCall()
 
-// function myMap(coords) {
-//   console.log('coords in my Map', coords)
-//   var mapProp = {
-//     center: new google.maps.LatLng(51.508742, -0.12085),
-//     zoom: 5,
-//   }
-//   var map = new google.maps.Map(
-//     document.getElementById('location-map'),
-//     mapProp
-//   )
-// }
+function myMap(coords) {
+  console.log('coords in my Map', coords)
+  var mapProp = {
+    center: new google.maps.LatLng(lat, lng),
+    zoom: 5,
+  }
+  var map = new google.maps.Map(
+    document.getElementById('location-map'),
+    mapProp
+  )
+}
